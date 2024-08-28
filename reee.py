37a975960f3b8a829e37ed52f93f0cfdaafbcfe3
Avery Lile <lileave000@warren.k12.in.us>
2024-08-28 09:37:32 -0400

    initial commit

---
+++ reee.py
@@ -0,0 +1,8 @@
+name = input("What is your name: ")
+#INSTRUCTION:  write an input
+# statement that asks for
+# number of droid and store as droids
+#INSTRUCTION:  write an input
+# statement that asks for
+# number of wookies and store as wookiees
+print(name, "wants to meet", "droids and ", "Wookiees!")
